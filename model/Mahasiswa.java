b4a243d8c701a314f0ee10c80d0cbac2814d31f6
fajrshadiq <fajrshadiq.re@gmail.com>
2023-04-05 12:28:24 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+ System.out.println();
+ System.out.println("== Data Mahasiswa =================");
+ System.out.println("NIS   : 2110010514");
+ System.out.println("Nama  : M. Fajar Shadiq Redhani");
+ System.out.println("===================================");
+
+ }
+}
